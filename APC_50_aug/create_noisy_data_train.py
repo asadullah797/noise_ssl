2bb1ca778178dec52edf65607bc25b6df00e1370
asadullah797 <asadullah797@gmail.com>
2023-04-29 22:07:32 +0100

    commiteed

---
+++ APC_50_aug/create_noisy_data_train.py
@@ -0,0 +1,75 @@
+import sys
+import math
+import os
+import shutil
+import glob
+import random
+import librosa
+import numpy as np
+from scipy.io.wavfile import write
+
+source = "train-clean-50"
+dest   = "train-clean50-aug50"
+sampling_rate=16000
+noises= glob.glob("musan/noise/free-sound/*.wav")
+
+def add_noise(x, s, snr_db):
+
+  # First let's match the length of the two signals
+  x_len = x.shape[0]
+  s_len = s.shape[0]
+
+  if x_len > s_len:
+    # Calculate how many times we need to repeat the signal and round up to the nearest integer
+    reps = math.ceil(x_len/s_len)
+
+    # Use the function np.tile to repeat an array
+    s = np.tile(s, reps)
+
+  # Truncate the background signal
+  s = s[:x_len]
+
+  # Check if the lengths are the same
+  assert x_len == s.shape[0]
+
+  # Convert SNRdb to linear
+  snr = 10**(snr_db/10)
+
+  # Estimate noise and signal power
+  sp = np.sqrt(np.mean(s**2))
+  xp = np.sqrt(np.mean(x**2))
+
+  # Calculate desired noise power based on the SNR value
+  sp_target = xp/snr
+
+  # Scale factor noise signal
+  alpha = sp_target/sp
+  print(f'alpha={alpha}')
+  # Sum speech and background noise
+  y = x + alpha*s
+  return y
+
+i=0
+count=0
+for readers in os.listdir(source):
+  for chapters in os.listdir(os.path.join(source,readers)):
+    for wav_file in os.listdir(os.path.join(source, readers, chapters)):
+      if not os.path.exists(os.path.join(dest,readers,chapters)): 
+       os.makedirs(os.path.join(dest, readers, chapters))
+      if wav_file.endswith('.flac'):
+          print('count==', count%len(noises))
+          noise_filename = noises[count%len(noises)]
+          count = count+1
+          x, _ = librosa.load(os.path.join(source, readers, chapters, wav_file), sr=sampling_rate, mono=True)
+          s, _ = librosa.load(noise_filename, sr=sampling_rate, mono=True)
+          snr_dbs = [5, 10, 15]
+          y = add_noise(x, s, snr_db=snr_dbs[i%len(snr_dbs)])
+          i=i+1       
+          new_wav_file= wav_file.split('/')[-1].split('.')[0]+"-aug"+".flac"
+          #print('new_wav_file', new_wav_file)
+          shutil.copy(os.path.join(source, readers, chapters, wav_file), os.path.join(dest, readers, chapters))
+          write(os.path.join(dest, readers, chapters, new_wav_file), sampling_rate, y) 
+      shutil.copy(os.path.join(source, readers, chapters, readers+"-"+chapters+".trans.txt"), os.path.join(dest, readers, chapters))
+
+print('i=', i)
+print('count=', count)
